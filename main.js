be10c1ca535ddc75807aba5484e6aec11e22a44a
saimon289 <37207504+saimon289@users.noreply.github.com>
2019-06-27 19:54:33 +0600

    dfghdhdh
    dfhgdh

---
+++ main.js
@@ -0,0 +1,12 @@
+var button = document.getElementById('sub');
+button.onclick = function () {
+	document.location.href = 'index2.html' ; 
+}
+
+var	sobaka = document.getElementById('sob');
+
+sobaka.onclick= function () {
+	alert('helo'); 
+}
+
+
---
+++ main2.js
@@ -0,0 +1,18 @@
+var	sobaka = document.getElementById('sob');
+
+sobaka.onclick=function  jump() {
+	
+	sobaka.style.bottom = 250+'px';
+	setTimeout(function() { 
+     sobaka.style.bottom = -28+'px';
+	}, 700);
+}
+
+
+
+         
+        
+
+var box = document.getElementById('box');
+
+ 
